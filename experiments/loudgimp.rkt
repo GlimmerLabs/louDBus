58ae6a4a700495d8d11bc745229ad9dbdf5073d5
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2014-01-17 10:40:41 -0600

    Add quick hack to test connecting to teh gimp.

---
+++ experiments/loudgimp.rkt
@@ -0,0 +1,9 @@
+#lang racket
+
+# A quick hack to see if I can connct to the GIMP.
+
+(require louDBus/unsafe)
+
+(define gimp (loudbus-proxy "edu.grinnell.cs.glimmer.GimpDBus"
+                            "/edu/grinnell/cs/glimmer/gimp"
+                            "edu.grinnell.cs.glimmer.pdb"))
